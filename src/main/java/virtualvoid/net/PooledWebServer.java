5870a12fdb32fe9c30e007b68d1c652193209337
Johannes Rudolph <johannes_rudolph@gmx.de>
2010-10-12 11:11:46 +0200

    add longish comment

--- src/main/java/virtualvoid/net/PooledWebServer.java
+++ src/main/java/virtualvoid/net/PooledWebServer.java
@@ -16,11 +16,29 @@ import java.util.concurrent.Executors;
 
 /**
  * A simple pooling web server. You can define the behaviour by setting executor and handler.
- * Variable executor specifies the pooling strategy to use. The handler is called in its own
+ * Field `executor` specifies the pooling strategy to use. The handler is called in its own
  * thread to handle an incoming connection.
  */
 public class PooledWebServer {
-    private final ExecutorService executor = Executors.newFixedThreadPool(1);
+    // This is the most important configuration to do and you would have to find
+    // out experimentally which is the setting for the pool.
+    // Without keep-alive and when the handling of request is mainly CPU-bound
+    // you would use about N threads (with N being the number of CPUs).
+    // When processing requests contains wait times (e.g. because of I/O) you
+    // would have to increase the number of threads to N*(1+WT/ST) with WT/ST being the ratio
+    // of wait time to service time.
+    //
+    // E.g. see Brian Goetz, Java theory and practice: Thread pools and work queues
+    // http://www.ibm.com/developerworks/java/library/j-jtp0730.html
+    //
+    // With keep-alive in place, configuration becomes harder because you have
+    // to figure out both, keep-alive timeouts and the number of threads. With a long too long timeout
+    // you might keep open many connections and threads waiting for requests and using system resources.
+    //
+    // One possibility is to decouple connections and threads by putting open, waiting connections into
+    // a list which is monitored for new data. When new data arrives the requests are scheduled again
+    // for processing. (not implemented here)
+    private final ExecutorService executor = Executors.newFixedThreadPool(2);
     private final Handler handler = new StaticHttpFileHandler(new File("www"));
     private final SocketAddress endpoint =
         new InetSocketAddress(8020);
